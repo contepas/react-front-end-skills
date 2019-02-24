04b8c7be6a492cd416fb0d5d82f72dd442b4b771
PaCo <conte.pas@me.com>
2019-02-24 15:16:53 +0100

    Refactor on the Provider. Bugfix on asking more times the same subject

--- src/App.js
+++ src/App.js
@@ -1,52 +1,26 @@
 import React, { Component } from 'react';
-import {Provider} from './components/Context';
-import server_response from './fakeData'
 import SearchForm from './components/SearchForm';
 import CourseList from './components/CourseList';
+import {Consumer} from './components/Context'
 
 
 class App extends Component {
-  state = {
-    matches: []
-  }
-
-  filterCourses = (subject) => {
-    const newMatches = [];
-
-    for (let course of server_response.COURSES) {
-      if (course.keys.includes(subject)) {
-        newMatches.push(course);
-      }
-    }
-
-    this.setState(prevState => {
-      console.log(prevState)
-      return {
-        matches: [...newMatches, ...prevState.matches]
-      }
-    })
-  }
-
-  renderCourseList = () => {
-    if (this.state.matches.length){
-      return <CourseList />
-    }
-  }
 
   render() {
     return (
-      <Provider value = {{
-                          matches: this.state.matches,
-                          actions: {
-                            filterCourses: this.filterCourses
-                          }
-                          }}>
-        <div className="App">
-          <h1>Click me to show or hide the search field</h1>
-            <SearchForm />
-            {this.renderCourseList()}
-        </div>
-      </Provider>
+      <Consumer>
+      {context => {
+        const totMatches = context.matches.length;
+        return (
+          <div className="App">
+            <h1>Click me to show or hide the search field</h1>
+              <SearchForm />
+              {totMatches ? <CourseList /> : null}
+          </div>
+        )
+      }}
+      
+      </Consumer>
     );
   }
 }
--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -1,6 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
+import server_response from '../../fakeData'
+
 
 const SearchCoursesContext = React.createContext(null);
 
-export const Provider = SearchCoursesContext.Provider;
+export class Provider extends Component {
+    state = {
+        matches: []
+    }
+
+    /**
+     * populate the matches array state, based on subject;
+     * if there is a match between 'subject' and keys values in COURSES,
+     * the relative course will be added to the matches array.
+     */
+    filterCourses = (subject) => {
+        const newMatches = [];
+    
+        for (let course of server_response.COURSES) {
+            if (course.keys.includes(subject)) {
+                newMatches.push(course);
+            }
+        }
+    
+        this.setState(prevState => {
+
+            for (let match of newMatches) {
+                if (prevState.matches.includes(match)) {
+                    return{
+                        matches: prevState.matches
+                    }
+                }
+            }
+            return {
+                matches: [...newMatches, ...prevState.matches]
+            }
+        })
+    }
+    
+
+    render(){
+        return (
+            <SearchCoursesContext.Provider value = {{
+                matches: this.state.matches,
+                actions: {
+                  filterCourses: this.filterCourses
+                }
+            }}>
+                {this.props.children}
+            </SearchCoursesContext.Provider>
+        );
+    }
+}
+
 export const Consumer = SearchCoursesContext.Consumer;
