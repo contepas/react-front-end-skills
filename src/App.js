bc137984842075490724f2cbdf88d7e7e756e286
PaCo <conte.pas@me.com>
2019-02-26 17:52:35 +0100

    Add component to handle 404 error

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 import CourseFinder from './components/CourseFinder';
- import Header from './components/Header';
-// import {Consumer} from './components/Context';
-
+import Header from './components/Header';
+import NotFound from './components/NotFound'
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
-      <Route path="/course-finder" component={CourseFinder} />
+      <Switch>
+        <Route path="/course-finder" component={CourseFinder} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 )
-
-// class App extends Component {
-
-//   render() {
-//     return (
-//       <Consumer>
-//       {({matches}) => {
-//         const totMatches = matches.length;
-//         return (
-//           <div className="App">
-//             <h1>Click me to show or hide the search field</h1>
-//               <SearchForm />
-//               {totMatches ? <CourseList /> : null}
-//           </div>
-//         )
-//       }}
-//       </Consumer>
-//     );
-//   }
-// }
 
 export default App;
