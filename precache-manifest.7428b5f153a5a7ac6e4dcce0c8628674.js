bd2fb661ae74622ae3f24c77e97e950b02307d9b
PaCo <conte.pas@me.com>
2019-03-03 03:06:01 +0100

    Updates

---
+++ precache-manifest.7428b5f153a5a7ac6e4dcce0c8628674.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5459900eab20a0d65a70358d279b6361",
+    "url": "/react-front-end-skills/static/media/moneta-0.5459900e.svg"
+  },
+  {
+    "revision": "b39e6a8cc5385dc409c1df93391eb149",
+    "url": "/react-front-end-skills/static/media/logo.b39e6a8c.svg"
+  },
+  {
+    "revision": "8ea3e70811e277381c40",
+    "url": "/react-front-end-skills/static/js/runtime~main.8ea3e708.js"
+  },
+  {
+    "revision": "f77c25f972d1525d151e",
+    "url": "/react-front-end-skills/static/js/main.f77c25f9.chunk.js"
+  },
+  {
+    "revision": "eec332de4d517e5ea50b",
+    "url": "/react-front-end-skills/static/js/2.eec332de.chunk.js"
+  },
+  {
+    "revision": "f77c25f972d1525d151e",
+    "url": "/react-front-end-skills/static/css/main.b58ff089.chunk.css"
+  },
+  {
+    "revision": "e49466c0bea02ea91a1a0d68219e372e",
+    "url": "/react-front-end-skills/index.html"
+  }
+];
